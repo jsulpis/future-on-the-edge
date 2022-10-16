eeb606f3f7cbbbcdf158aca3ab92977a5bb70503
Julien Sulpis <julien.sulpis@gmail.com>
2022-10-16 14:22:42 +0200

    feat(ssr): add query param for the api to use

--- apps/app-edge/src/routes/+page.server.js
+++ apps/app-edge/src/routes/+page.server.js
@@ -1,13 +1,16 @@
 import { env } from "$env/dynamic/private";
-import { API_URL } from "$env/static/private";
+import { API_BASE_URL } from "$env/static/private";
 
 export async function load({ url }) {
   // SSR data
   const name = url.searchParams.get("name");
   const ssrRegion = env.AWS_REGION || "✨ Edge ✨";
 
+  const apiParam = url.searchParams.get("api");
+  const apiRelativeUrl = apiParam ? `/date-${apiParam}` : "/date";
+
   // API data
-  const apiResponse = await fetch(API_URL);
+  const apiResponse = await fetch(API_BASE_URL + apiRelativeUrl);
   const apiCacheStatus = apiResponse.headers.get("x-vercel-cache");
 
   const { region: apiRegion, timestamp } = await apiResponse.json();
