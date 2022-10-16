93c38a7435d13a0ec757175cbc828e6e9f5583ce
Julien Sulpis <julien.sulpis@gmail.com>
2022-10-16 16:18:17 +0200

    feat(slides): add astro project

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-# Little hack to deploy all the apps in a single Vercel project under different urls
+# Script to deploy all the apps in a single Vercel project under different urls
+# using Vercel Build Output API (https://vercel.com/docs/build-output-api/v3)
 
 find . -type d -name output -exec rm -rf {} +
 mkdir -p .vercel/output/functions
 
 pnpm run -r build
 
-for appName in $(ls ./apps) 
+for appName in $(ls ./apps)
 do
     mkdir -p .vercel/output/static/$appName
     cp -r apps/$appName/.vercel/output/functions/render.func .vercel/output/functions/$appName.func
@@ -15,6 +16,9 @@ do
 done
 
 cp -r apps/app-history-spa/dist/* .vercel/output/static/app-history-spa
+
+mkdir -p .vercel/output/static/slides
+cp -r slides/dist/* .vercel/output/static/slides
 
 cp config.json .vercel/output/config.json
 cp public/index.html .vercel/output/static/index.html 
