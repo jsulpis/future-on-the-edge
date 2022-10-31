e8eea1bbb6d20a7c659d3105ab0df79e63cbde1a
Julien Sulpis <julien.sulpis@gmail.com>
2022-10-31 16:54:30 +0100

    docs: add comments in build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,14 @@
 # Script to deploy all the apps in a single Vercel project under different urls
 # using Vercel Build Output API (https://vercel.com/docs/build-output-api/v3)
 
+# Clean
 find . -type d -name output -exec rm -rf {} +
 mkdir -p .vercel/output/functions
 
+# Build
 pnpm run -r build
 
+# Move bundles in Vercel output folder
 for appName in $(ls ./apps)
 do
     mkdir -p .vercel/output/static/$appName
@@ -15,10 +18,12 @@ do
     cp -r apps/$appName/.vercel/output/static/_app .vercel/output/static/$appName/_app
 done
 
+# For the SPA demo and the slides, the build output is different (dist folder)
 cp -r apps/app-history-spa/dist/* .vercel/output/static/app-history-spa
 
 mkdir -p .vercel/output/static/slides
 cp -r slides/dist/* .vercel/output/static/slides
 
+# Replace the config file and add a root HTML document
 cp config.json .vercel/output/config.json
 cp public/index.html .vercel/output/static/index.html 
